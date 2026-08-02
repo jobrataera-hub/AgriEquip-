93d2a4184c1a1cfe0a2488cd7781b27163a3591b
jobrataera-hub <jobrataera@gmail.com>
2026-08-02 15:30:13 +0300

    Restore root firebase.js
    Signed-off-by: jobrataera-hub <jobrataera@gmail.com>

---
+++ firebase.js
@@ -0,0 +1,22 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js';
+import { getAuth, setPersistence, browserLocalPersistence } from 'https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js';
+import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDQL4d38G1ZQwARORaszwsf1YIRypKjP_M",
+  authDomain: "agriequip-8124b.firebaseapp.com",
+  projectId: "agriequip-8124b",
+  storageBucket: "agriequip-8124b.appspot.com",
+  messagingSenderId: "662047538853",
+  appId: "1:662047538853:web:84dd49584bface153a174c"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+setPersistence(auth, browserLocalPersistence).catch(e =>
+  console.warn('Auth persistence:', e)
+);
+
+export { auth, db, app };
